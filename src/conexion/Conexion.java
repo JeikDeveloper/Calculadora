5cdbff96570b13506247ce7732ca539c2959bccf
Jeiber <jeiber2001@gmail.com>
2022-03-27 21:19:05 -0500

    feax: Inicio de proyecto calculadora app movil

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,20 @@
+package conexion;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    Connection con;
+    public Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/historial","root","");
+        } catch (Exception e) {
+            System.err.println("Error:" +e);
+        }
+    }
+    
+    public Connection getConnection() {
+        return con;
+    }
+}
